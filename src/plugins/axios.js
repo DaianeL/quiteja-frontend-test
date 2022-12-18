5718a48add8b7dcd4a5a15bbcbcba7abf7bccce2
daianelima <lima_dai@outlook.com>
2022-12-18 18:40:23 -0300

    Configuração App id

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -3,7 +3,7 @@ import axios from 'axios'
 const instance = axios.create({
   baseURL: 'https://dummyapi.io/data/v1',
   headers: {
-    'app-id': '639e28615ec180400d2578e9'
+    'app-id': process.env.VUE_APP_ID
   }
 })
 
